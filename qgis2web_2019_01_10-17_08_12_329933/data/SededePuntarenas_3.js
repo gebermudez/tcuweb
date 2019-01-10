be76d950b5d69640208c18e4b10f7764935291a5
gebermudez <30710178+gebermudez@users.noreply.github.com>
2019-01-10 17:58:08 -0600

    Add files via upload

---
+++ qgis2web_2019_01_10-17_08_12_329933/data/SededePuntarenas_3.js
@@ -0,0 +1,12 @@
+var json_SededePuntarenas_3 = {
+"type": "FeatureCollection",
+"name": "SededePuntarenas_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Provincia": "Puntarenas", "Cantón": "Puntarenas", "Distrito": "Puntarenas", "Nombre": "Ciudad Puntarenas hacia la Carbono Neutralidad 2021", "Sede": "Sede de Puntarenas", "Código": "TCU-103", "Área": "Ambiente y Desarrollo Sostenible" }, "geometry": { "type": "Point", "coordinates": [ -84.826072082812388, 9.978276084897207 ] } },
+{ "type": "Feature", "properties": { "Provincia": "Puntarenas", "Cantón": "Puntarenas", "Distrito": "Puntarenas", "Nombre": "Ciudad Puntarenas hacia la Carbono Neutralidad 2021", "Sede": "Sede de Puntarenas", "Código": "TCU-103", "Área": "Ambiente y Desarrollo Sostenible" }, "geometry": { "type": "Point", "coordinates": [ -85.033225933971167, 10.000801052103382 ] } },
+{ "type": "Feature", "properties": { "Provincia": "Puntarenas", "Cantón": "Puntarenas", "Distrito": "Puntarenas", "Nombre": "Ciudad Puntarenas hacia la Carbono Neutralidad 2021", "Sede": "Sede de Puntarenas", "Código": "TCU-103", "Área": "Ambiente y Desarrollo Sostenible" }, "geometry": { "type": "Point", "coordinates": [ -84.983932738195861, 9.987571736270262 ] } },
+{ "type": "Feature", "properties": { "Provincia": "Puntarenas", "Cantón": "Puntarenas", "Distrito": "Puntarenas", "Nombre": "Ciudad Puntarenas hacia la Carbono Neutralidad 2021", "Sede": "Sede de Puntarenas", "Código": "TCU-103", "Área": "Ambiente y Desarrollo Sostenible" }, "geometry": { "type": "Point", "coordinates": [ -84.771468838050964, 10.014336132589584 ] } },
+{ "type": "Feature", "properties": { "Provincia": "Puntarenas", "Cantón": "Puntarenas", "Distrito": "Puntarenas", "Nombre": "Ciudad Puntarenas hacia la Carbono Neutralidad 2021", "Sede": "Sede de Puntarenas", "Código": "TCU-103", "Área": "Ambiente y Desarrollo Sostenible" }, "geometry": { "type": "Point", "coordinates": [ -84.905205758014574, 9.940691721786246 ] } }
+]
+}
